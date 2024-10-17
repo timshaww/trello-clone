9a923fc23c6e04e8559d56515382ff9e69722947
timshaww <time4shaw@gmail.com>
2024-10-17 11:22:52 -0400

    test commit

--- src/app/_components/List.tsx
+++ src/app/_components/List.tsx
@@ -9,6 +9,8 @@ interface ListProps {
 	setList: React.Dispatch<React.SetStateAction<ListType>>;
 }
 
+const test = 'test';
+
 const List: React.FC<ListProps> = ({ list, setList }) => {
 	const [cards, setCards] = useState(list.cards);
 
