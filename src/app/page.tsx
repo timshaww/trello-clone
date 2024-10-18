1ae17cd8d55375e9f47f9f6f4e387e15257f09a5
timshaww <time4shaw@gmail.com>
2024-10-17 21:03:47 -0400

    little fix!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
 	const router = useRouter();
 
 	useEffect(() => {
 		router.push('/b/board-1');
-	}, []);
+	}, [router]);
+
 	return <div className='flex flex-row w-screen h-screen items-center justify-center'></div>;
 }
